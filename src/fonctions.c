41b974c33249df32d1e5c396c7fe5fbb74b64dd7
Alex Barféty <alexbarfety@free.fr>
2023-11-21 00:24:59 +0100

    Enable gcc warnings & errors

--- src/fonctions.c
+++ src/fonctions.c
@@ -8,7 +8,7 @@ int compter_tracks (FILE *bpm)
     int nb_tracks = 0 ;
     char stok = ' ' ;
     rewind (bpm) ;
-    while ( (stok = getc (bpm)) != EOF)
+    while ( (stok = (char) getc (bpm)) != EOF)
         {
             if (stok == '\n')
                 {
@@ -21,23 +21,21 @@ int compter_tracks (FILE *bpm)
 //Fonction qui écrit les tags d'un chanson dans le .csv :
 void ecrire_tags (FILE *liste, FILE *bpm)
 {
-    int n_bpm = 0 ;
-    float f_bpm = 0.0 ;
+    double f_bpm = 0.0 ;
     char titre[1000] = "", artiste[1000] = "" ;
 
     avancer_fichier (bpm) ; //On fait avancer le curseur virtuel jusqu'à avoir passer le chemin du mp3.
     fscanf_tab (bpm, titre) ; //On scanne le titre.
     fscanf_tab (bpm, artiste) ; //On scanne l'artiste.
-    fscanf (bpm, "%f\n", &f_bpm) ; //On scanne le BPM.
+    fscanf (bpm, "%lf\n", &f_bpm) ; //On scanne le BPM.
     arrondir (&f_bpm, 2) ; //On arrondit les BPM.
-    n_bpm = (int) f_bpm ;
     fprintf (liste, "%s; %s; %f\n", artiste, titre, f_bpm) ; //On écrit le tout dans le fichier .csv.
 }
 
-//Fonction qui arrondit un float :
-void arrondir (float *mise, int precision)
+//Fonction qui arrondit un double :
+void arrondir (double *mise, int precision)
 {
-    float test = pow (10, precision) * *mise ;
+    double test = pow (10, precision) * *mise ;
     if (test - (int) test >= 0.5)
         {
             *mise = ( (int) test + 1) / pow (10, precision) ;
@@ -57,7 +55,7 @@ void avancer_fichier (FILE *fichier)
     //On prends les premier caractères pour tester une première fois :
     for (i = 0 ; i < 4 ; i++)
         {
-            stock[i] = getc (fichier) ;
+            stock[i] = (char) getc (fichier) ;
         }
     stock[4] = '\0' ;
 
@@ -67,16 +65,16 @@ void avancer_fichier (FILE *fichier)
                 {
                     stock[i] = stock[i+1] ;
                 }
-            stock[3] = getc (fichier) ;
+            stock[3] = (char) getc (fichier) ;
         }
-    stock[0] = getc (fichier) ;
+    stock[0] = (char) getc (fichier) ;
 }
 
 //Fonction qui scanne le titre (ou l'artiste) en entier jusqu'à la pochaine tabulation
 void fscanf_tab (FILE *fichier, char champs[])
 {
     char stock = ' ' ;
-    while ( (stock = getc (fichier)) != '\t')
+    while ( (stock = (char) getc (fichier)) != '\t')
         {
             sprintf (champs, "%s%c", champs, stock) ;
         }
--- src/fonctions.h
+++ src/fonctions.h
@@ -6,7 +6,7 @@
 // Prototype des fonctions :
 int compter_tracks (FILE *bpm) ;
 void ecrire_tags (FILE *liste, FILE *bpm) ;
-void arrondir (float *mise, int precision) ;
+void arrondir (double *mise, int precision) ;
 void avancer_fichier(FILE *fichier) ;
 void fscanf_tab (FILE *fichier, char champs[]) ;
 
